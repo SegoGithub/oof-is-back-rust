a77a78eae19527afbfdc4ece43c2b26d606374bb
Sego <sego@sego.app>
2022-11-24 18:43:01 +0200

    Add Linux Support

--- src/crossplatform.rs
+++ src/crossplatform.rs
@@ -24,60 +24,15 @@ pub fn path() {
     for version in versions {
         // check if RobloxPlayerBeta.exe exists
         if fs::metadata(version.clone() + "\\RobloxPlayerBeta.exe").is_ok() {
-            println!(
-                "{} Roblox Path was detected as: {}",
-                style("info").cyan(),
-                version
-            );
-            return;
+            return (version.to_string(), vec![]);
         }
     }
+    return ("".to_string(), vec![]);
 }
 
 #[cfg(target_os = "windows")]
 pub fn play_sound() {
-    use std::{env, fs};
-
-    static USERNAME: &str = "USERNAME";
-    // list all the files in %localappdata%\Roblox\Versions
-    let mut versions = vec![];
-    for entry in fs::read_dir(
-        "C:\\Users\\".to_string()
-            + &env::var(USERNAME).unwrap()
-            + "\\AppData\\Local\\Roblox\\Versions",
-    )
-    .unwrap()
-    {
-        versions.push(entry.unwrap().path().to_str().unwrap().to_string());
-    }
-    // sort the list of versions
-    versions.sort();
-    // reverse the list of versions
-    versions.reverse();
-    // print the list of versions
-    for version in versions {
-        // check if RobloxPlayerBeta.exe exists
-        if fs::metadata(version.clone() + "\\RobloxPlayerBeta.exe").is_ok() {
-            use rodio::{source::Source, Decoder, OutputStream};
-            use std::fs::File;
-            use std::io::BufReader;
-
-            // Get a output stream handle to the default physical sound device
-            let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-            // Load a sound from a file, using a path relative to Cargo.toml
-            let file = BufReader::new(
-                File::open(version.to_string() + &"\\content\\sounds\\ouch.ogg").unwrap(),
-            );
-            // Decode that sound file into a source
-            let source = Decoder::new(file).unwrap();
-            // Play the sound directly on the device
-            stream_handle.play_raw(source.convert_samples());
-
-            // The sound plays in a separate audio thread,
-            // so we need to keep the main thread alive while it's playing.
-            std::thread::sleep(std::time::Duration::from_secs(5));
-        }
-    }
+    open::that(path().0.to_string() + &"\\content\\sounds\\ouch.ogg").unwrap();
 }
 
 #[cfg(target_os = "windows")]
@@ -276,7 +231,315 @@ pub fn custom_sound() {
     }
 }
 
-#[cfg(target_os = "windows")]
-pub fn fix_terminal() {
-    std::thread::sleep(std::time::Duration::from_millis(100));
+// Linux
+#[cfg(target_os = "linux")]
+pub fn path() -> (String, Vec<()>) {
+    use std::fs;
+    // list all the files in ~/.local/share/grapejuice/prefixes/player/drive_c/users/USERNAME/AppData/Local/Roblox/Versions
+    let mut versions = vec![];
+    for entry in fs::read_dir(
+        "/home/".to_string()
+            + &whoami::username()
+            + "/.local/share/grapejuice/prefixes/player/drive_c/users/"
+            + &whoami::username()
+            + "/AppData/Local/Roblox/Versions",
+    )
+    .unwrap()
+    {
+        versions.push(entry.unwrap().path().to_str().unwrap().to_string());
+    }
+    // sort the list of versions
+    versions.sort();
+    // reverse the list of versions
+    versions.reverse();
+    // print the list of versions
+    for version in versions {
+        // check if RobloxPlayerBeta.exe exists
+        if fs::metadata(version.clone() + "/RobloxPlayerBeta.exe").is_ok() {
+            return (version.to_string(), vec![]);
+        }
+    }
+    return ("".to_string(), vec![]);
+    // I DO NOT KNOW RUST
+    // I DO NOT KNOW WHY THIS WORKS
+}
+
+#[cfg(target_os = "linux")]
+pub fn play_sound() {
+    open::that(path().0.to_string() + &"/content/sounds/ouch.ogg").unwrap();
+}
+
+#[cfg(target_os = "linux")]
+pub fn replace_sound(friendly_name: String, sound: String) {
+    println!("You chose {}", friendly_name);
+    use std::{
+        fs::{self, File},
+        io::{self, Write},
+        os::unix::prelude::PermissionsExt,
+        process::Command,
+    };
+    // delete a file
+    fs::remove_file(path().0.to_string() + "/content/sounds/ouch.ogg").unwrap();
+    println!("[1/2] Deleted existing death sound");
+    // copy a file
+    fs::copy(
+        "sounds/".to_string() + &sound + "/ouch.ogg",
+        path().0.to_string() + "/content/sounds/ouch.ogg",
+    )
+    .unwrap();
+    println!("[2/2] Copied {} sound", friendly_name);
+    println!("✅ Done!");
+    // ASK USER A Y/N QUESTION
+    println!("Would you like to prevent Roblox from replacing your Oof sound? (y/n)");
+    let mut autostart = String::new();
+    io::stdin().read_line(&mut autostart).unwrap();
+    if autostart.trim() == "y" {
+        if !fs::metadata("/home/".to_string() + &whoami::username() + "/.config/systemd/user")
+            .is_ok()
+        {
+            fs::create_dir_all(
+                "/home/".to_string() + &whoami::username() + "/.config/systemd/user",
+            )
+            .unwrap();
+        }
+
+        if !fs::metadata("/home/".to_string() + &whoami::username() + "/.oof-is-back").is_ok() {
+            fs::create_dir_all("/home/".to_string() + &whoami::username() + "/.oof-is-back")
+                .unwrap();
+        }
+
+        // check if file exists
+        if !fs::metadata(path().0.to_string() + "/content/sounds/.ouch").is_ok() {
+            fs::File::create(path().0.to_string() + "/content/sounds/.ouch").unwrap();
+        }
+
+        if fs::metadata("/home/".to_string() + &whoami::username() + "/.oof-is-back/ouch.ogg")
+            .is_ok()
+        {
+            // delete a file
+            fs::remove_file("/home/".to_string() + &whoami::username() + "/.oof-is-back/ouch.ogg")
+                .unwrap();
+        }
+
+        fs::copy(
+            "sounds/".to_string() + &sound + "/ouch.ogg",
+            "/home/".to_string() + &whoami::username() + "/.oof-is-back/ouch.ogg",
+        )
+        .unwrap();
+
+        if !fs::metadata(
+            "/home/".to_string() + &whoami::username() + &"/.oof-is-back/oof-is-back2".to_string(),
+        )
+        .is_ok()
+        {
+            let mut resp = reqwest::blocking::get(
+                "https://github.com/SegoGithub/oof-is-back-autostart/releases/download/v2.1.0/oof-is-back2",
+            )
+            .unwrap();
+            let mut out = File::create(
+                "/home/".to_string()
+                    + &whoami::username()
+                    + &"/.oof-is-back/oof-is-back2".to_string(),
+            )
+            .unwrap();
+            io::copy(&mut resp, &mut out).unwrap();
+            // make the file executable
+            fs::set_permissions(
+                "/home/".to_string()
+                    + &whoami::username()
+                    + &"/.oof-is-back/oof-is-back2".to_string(),
+                fs::Permissions::from_mode(0o755),
+            )
+            .unwrap();
+        };
+        if !fs::metadata("/usr/bin/oof-is-back/icon.png".to_string()).is_ok() {
+            let mut resp = reqwest::blocking::get(
+                "https://github.com/SegoGithub/oof-is-back/raw/main/icon.png",
+            )
+            .unwrap();
+            let mut out = File::create(
+                "/home/".to_string() + &whoami::username() + &"/.oof-is-back/icon.png".to_string(),
+            )
+            .unwrap();
+            io::copy(&mut resp, &mut out).unwrap();
+        };
+        if !fs::metadata(
+            "/home/".to_string()
+                + &whoami::username()
+                + "/.config/systemd/user/oof-is-back.service",
+        )
+        .is_ok()
+        {
+            let mut file = File::create(
+                "/home/".to_string()
+                    + &whoami::username()
+                    + "/.config/systemd/user/oof-is-back.service",
+            )
+            .unwrap();
+            let systemd = "[Unit]\nDescription=oof-is-back, preventing Roblox from replacing your oof sound\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/".to_string() + &whoami::username().to_string() +"/.oof-is-back/oof-is-back2";
+            // write systemd file
+            file.write_all(systemd.as_bytes()).unwrap();
+        };
+        Command::new("systemctl")
+            .arg("--user")
+            .arg("enable")
+            .arg("oof-is-back")
+            .output()
+            .expect("failed to run: systemctl --user enable oof-is-back");
+        // start the oof-is-back service
+        Command::new("systemctl")
+            .arg("--user")
+            .arg("start")
+            .arg("oof-is-back")
+            .output()
+            .expect("failed to run: systemctl --user start oof-is-back");
+        println!("You can disable this feature by running `systemctl --user disable oof-is-back`");
+    };
+
+    println!("✅ Done!");
+    println!("Press any key to continue...");
+    let mut autostart = String::new();
+    io::stdin().read_line(&mut autostart).unwrap();
+}
+
+#[cfg(target_os = "linux")]
+pub fn custom_sound() {
+    println!("You chose a custom sound");
+    use std::{
+        fs::{self, File},
+        io::{self, Write},
+        os::unix::prelude::PermissionsExt,
+        process::Command,
+    };
+    // create a folder in ./
+    fs::create_dir_all("custom_sound").unwrap();
+    println!("A file has been created in the current directory called custom_sound\nPlease put your custom sound in there and name it ouch.ogg");
+    // ASK USER A Y/N QUESTION
+    println!(
+        "Have you put your OGG audio file in the custom_sound folder and named it ouch.ogg? (y/n)"
+    );
+    let mut custom = String::new();
+    io::stdin().read_line(&mut custom).unwrap();
+
+    // delete a file
+    fs::remove_file(path().0 + "/content/sounds/ouch.ogg").unwrap();
+    println!("[1/2] Deleted existing death sound");
+    // copy a file
+    fs::copy(
+        "custom_sound/ouch.ogg",
+        path().0 + "/content/sounds/ouch.ogg",
+    )
+    .unwrap();
+    println!("[2/2] Copied custom sound");
+    println!("✅ Done!");
+    // ASK USER A Y/N QUESTION
+    println!("Would you like to prevent Roblox from replacing your Oof sound? (y/n)");
+    let mut autostart = String::new();
+    io::stdin().read_line(&mut autostart).unwrap();
+    if autostart.trim() == "y" {
+        if !fs::metadata("/home/".to_string() + &whoami::username() + "/.config/systemd/user")
+            .is_ok()
+        {
+            fs::create_dir_all(
+                "/home/".to_string() + &whoami::username() + "/.config/systemd/user",
+            )
+            .unwrap();
+        }
+
+        if !fs::metadata("/home/".to_string() + &whoami::username() + "/.oof-is-back").is_ok() {
+            fs::create_dir_all("/home/".to_string() + &whoami::username() + "/.oof-is-back")
+                .unwrap();
+        }
+
+        // check if file exists
+        if !fs::metadata(path().0.to_string() + "/content/sounds/.ouch").is_ok() {
+            fs::File::create(path().0.to_string() + "/content/sounds/.ouch").unwrap();
+        }
+
+        if fs::metadata("/home/".to_string() + &whoami::username() + "/.oof-is-back/ouch.ogg")
+            .is_ok()
+        {
+            // delete a file
+            fs::remove_file("/home/".to_string() + &whoami::username() + "/.oof-is-back/ouch.ogg")
+                .unwrap();
+        }
+
+        fs::copy(
+            "custom_sound/ouch.ogg",
+            "/home/".to_string() + &whoami::username() + "/.oof-is-back/ouch.ogg",
+        )
+        .unwrap();
+
+        if !fs::metadata(
+            "/home/".to_string() + &whoami::username() + &"/.oof-is-back/oof-is-back2".to_string(),
+        )
+        .is_ok()
+        {
+            let mut resp = reqwest::blocking::get(
+                "https://github.com/SegoGithub/oof-is-back-autostart/releases/download/v2.1.0/oof-is-back2",
+            )
+            .unwrap();
+            let mut out = File::create(
+                "/home/".to_string()
+                    + &whoami::username()
+                    + &"/.oof-is-back/oof-is-back2".to_string(),
+            )
+            .unwrap();
+            io::copy(&mut resp, &mut out).unwrap();
+            // make the file executable
+            fs::set_permissions(
+                "/home/".to_string()
+                    + &whoami::username()
+                    + &"/.oof-is-back/oof-is-back2".to_string(),
+                fs::Permissions::from_mode(0o755),
+            )
+            .unwrap();
+        };
+        if !fs::metadata("/usr/bin/oof-is-back/icon.png".to_string()).is_ok() {
+            let mut resp = reqwest::blocking::get(
+                "https://github.com/SegoGithub/oof-is-back/raw/main/icon.png",
+            )
+            .unwrap();
+            let mut out = File::create(
+                "/home/".to_string() + &whoami::username() + &"/.oof-is-back/icon.png".to_string(),
+            )
+            .unwrap();
+            io::copy(&mut resp, &mut out).unwrap();
+        };
+        if !fs::metadata(
+            "/home/".to_string()
+                + &whoami::username()
+                + "/.config/systemd/user/oof-is-back.service",
+        )
+        .is_ok()
+        {
+            let mut file = File::create(
+                "/home/".to_string()
+                    + &whoami::username()
+                    + "/.config/systemd/user/oof-is-back.service",
+            )
+            .unwrap();
+            let systemd = "[Unit]\nDescription=oof-is-back, preventing Roblox from replacing your oof sound\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/".to_string() + &whoami::username().to_string() +"/.oof-is-back/oof-is-back2";
+            // write systemd file
+            file.write_all(systemd.as_bytes()).unwrap();
+        };
+        Command::new("systemctl")
+            .arg("--user")
+            .arg("enable")
+            .arg("oof-is-back")
+            .output()
+            .expect("failed to run: systemctl --user enable oof-is-back");
+        // start the oof-is-back service
+        Command::new("systemctl")
+            .arg("--user")
+            .arg("start")
+            .arg("oof-is-back")
+            .output()
+            .expect("failed to run: systemctl --user start oof-is-back");
+        println!("You can disable this feature by running `systemctl --user disable oof-is-back`");
+    }
+    println!("✅ Done!");
+    println!("Press any key to continue...");
+    let mut autostart = String::new();
+    io::stdin().read_line(&mut autostart).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -2,133 +2,150 @@
 
 use serde_json::Value;
 
-#[cfg(target_os = "windows")]
 mod crossplatform;
 
 fn main() {
     // fix for windows terminal closing
     #[cfg(target_os = "windows")]
-    crossplatform::fix_terminal();
+    std::thread::sleep(std::time::Duration::from_millis(100));
 
     // wait for user to enter a number
     crossplatform::path();
     fn main_menu() {
-        console::Term::clear_screen(&console::Term::stdout());
-        crossplatform::path();
+        console::Term::clear_screen(&console::Term::stdout()).unwrap();
+        println!(
+            "Your Roblox Path was detected as: {:#?}",
+            crossplatform::path().0
+        );
         println!("Enter a number [1-4]:");
         println!("[1] ▶️ Play Current Death Sound\n[2] ☠️ Choose a New Death Sound\n[3] ⬆️ Check for Updates\n[4] ❌ Exit");
         let mut input = String::new();
         std::io::stdin()
             .read_line(&mut input)
             .expect("Failed to read line");
-        let mut input: u32 = input.trim().parse().expect("Please type a number!");
+        let input: u32 = input.trim().parse().expect("Please type a number!");
         handle_input(input)
     }
     main_menu();
     // if user enters 1, play current death sound
     fn handle_input(input: u32) {
-        if input == 1 {
-            println!("Playing current death sound...");
-            crossplatform::play_sound();
-            main_menu()
-        } else if input == 2 {
-            choose_sound()
-        } else if input == 3 {
-            println!("Checking for updates...");
-            // get current version
-            let current_version = env!("CARGO_PKG_VERSION");
-            println!("Current version: {}", current_version);
-            // get latest version from github api
-            let client = reqwest::blocking::Client::new();
-            let res = client
-                .get("https://api.github.com/repos/SegoGithub/oof-is-back-rust/releases/latest")
-                .header("User-Agent", "DeathSound")
-                .send()
-                .unwrap();
-            let json: Value = serde_json::from_str(&res.text().unwrap()).unwrap();
-            let latest_version = json["tag_name"].to_string();
-            let latest_version = latest_version.replace("\"", "");
-            println!("Latest version: {}", latest_version);
-            // compare versions
-            if current_version == latest_version {
-                println!("You are up to date!\nPress enter to continue...");
-                std::io::stdin()
-                    .read_line(&mut String::new())
-                    .expect("Failed to read line");
-                main_menu()
-            } else {
-                println!("There is an update available!\nDownload it here: https://github.com/SegoGithub/oof-is-back-rust/releases/latest\nPress enter to continue...");
-                std::io::stdin()
-                    .read_line(&mut String::new())
-                    .expect("Failed to read line");
+        match input {
+            1 => {
+                println!("Playing current death sound...");
+                crossplatform::play_sound();
                 main_menu()
             }
-
-        } else if input == 4 {
-            println!("Exiting...");
-            std::process::exit(0);
-        } else {
-            println!("Invalid input!\nReturning to main menu in 3 seconds...");
-            std::thread::sleep(std::time::Duration::from_secs(3));
-            main_menu()
+            2 => choose_sound(),
+            3 => {
+                println!("Checking for updates...");
+                // get current version
+                let current_version = env!("CARGO_PKG_VERSION");
+                println!("Current version: {}", current_version);
+                // get latest version from github api
+                let client = reqwest::blocking::Client::new();
+                let res = client
+                    .get("https://api.github.com/repos/SegoGithub/oof-is-back-rust/releases/latest")
+                    .header("User-Agent", "DeathSound")
+                    .send()
+                    .unwrap();
+                let json: Value = serde_json::from_str(&res.text().unwrap()).unwrap();
+                let latest_version = json["tag_name"].to_string().replace("\"", "");
+                println!("Latest version: {}", latest_version);
+                // compare versions
+                if current_version == latest_version {
+                    println!("You are up to date!\nPress enter to continue...");
+                    std::io::stdin()
+                        .read_line(&mut String::new())
+                        .expect("Failed to read line");
+                    main_menu()
+                } else {
+                    println!("There is an update available!\nDownload it here: https://github.com/SegoGithub/oof-is-back-rust/releases/latest\nPress enter to continue...");
+                    std::io::stdin()
+                        .read_line(&mut String::new())
+                        .expect("Failed to read line");
+                    main_menu()
+                }
+            }
+            4 => std::process::exit(0),
+            _ => {
+                println!("Invalid input!\nReturning to main menu in 3 seconds...");
+                std::thread::sleep(std::time::Duration::from_secs(3));
+                main_menu()
+            }
         }
     }
     fn choose_sound() {
-        console::Term::clear_screen(&console::Term::stdout());
+        console::Term::clear_screen(&console::Term::stdout()).unwrap();
         println!("Choose a new death sound\nEnter a number [1-9]:\n[1] Oof\n[2] Vine Boom Sound Effect (Bass Boosted)\n[3] GAH DAM\n[4] Half Life\n[5] Old Minecraft Death Sound\n[6] Lego Yoda Death\n[7] AUUUUUUGHHH\n[8] Custom Sound\n[9] Return to main menu");
         let mut input = String::new();
         std::io::stdin()
             .read_line(&mut input)
             .expect("Failed to read line");
-        let mut input: u32 = input.trim().parse().expect("Please type a number!");
+        let input: u32 = input.trim().parse().expect("Please type a number!");
         handle_sound_input(input)
     }
     fn handle_sound_input(input: u32) {
-        if input == 1 {
-            crossplatform::replace_sound("Oof".to_string(), "oof".to_string());
-            println!("Returning to main menu in 2 seconds..");
-            std::thread::sleep(std::time::Duration::from_secs(2));
-            main_menu()
-        } else if input == 2 {
-            crossplatform::replace_sound("Vine Boom Sound Effect (Bass Boosted)".to_string(), "vineboom".to_string());
-            println!("Returning to main menu in 2 seconds..");
-            std::thread::sleep(std::time::Duration::from_secs(2));
-            main_menu()
-        } else if input == 3 {
-            crossplatform::replace_sound("GAH DAM".to_string(), "gahdam".to_string());
-            println!("Returning to main menu in 2 seconds..");
-            std::thread::sleep(std::time::Duration::from_secs(2));
-            main_menu()
-        } else if input == 4 {
-            crossplatform::replace_sound("Half Life".to_string(), "hl".to_string());
-            println!("Returning to main menu in 2 seconds..");
-            std::thread::sleep(std::time::Duration::from_secs(2));
-            main_menu()
-        } else if input == 5 {
-            crossplatform::replace_sound("Old Minecraft Death Sound".to_string(), "mc".to_string());
-            println!("Returning to main menu in 2 seconds..");
-            std::thread::sleep(std::time::Duration::from_secs(2));
-            main_menu()
-        } else if input == 6 {
-            crossplatform::replace_sound("Lego Yoda Death".to_string(), "yoda".to_string());
-            println!("Returning to main menu in 2 seconds..");
-            std::thread::sleep(std::time::Duration::from_secs(2));
-            main_menu()
-        } else if input == 7 {
-            crossplatform::replace_sound("AUUUUUUGHHH".to_string(), "augh".to_string());
-            println!("Returning to main menu in 2 seconds..");
-            std::thread::sleep(std::time::Duration::from_secs(2));
-            main_menu()
-        } else if input == 8 {
-            crossplatform::custom_sound();
-            println!("Returning to main menu in 2 seconds..");
-            std::thread::sleep(std::time::Duration::from_secs(2));
-        } else if input == 9 {
-            main_menu()
-        } else {
-            println!("Invalid input!\nReturning to main menu in 3 seconds...");
-            std::thread::sleep(std::time::Duration::from_secs(3));
-            main_menu()
+        match input {
+            1 => {
+                crossplatform::replace_sound("Oof".to_string(), "oof".to_string());
+                println!("Returning to main menu in 2 seconds..");
+                std::thread::sleep(std::time::Duration::from_secs(2));
+                main_menu()
+            }
+            2 => {
+                crossplatform::replace_sound(
+                    "Vine Boom Sound Effect (Bass Boosted)".to_string(),
+                    "vineboom".to_string(),
+                );
+                println!("Returning to main menu in 2 seconds..");
+                std::thread::sleep(std::time::Duration::from_secs(2));
+                main_menu()
+            }
+            3 => {
+                crossplatform::replace_sound("GAH DAM".to_string(), "gahdam".to_string());
+                println!("Returning to main menu in 2 seconds..");
+                std::thread::sleep(std::time::Duration::from_secs(2));
+                main_menu()
+            }
+            4 => {
+                crossplatform::replace_sound("Half Life".to_string(), "hl".to_string());
+                println!("Returning to main menu in 2 seconds..");
+                std::thread::sleep(std::time::Duration::from_secs(2));
+                main_menu()
+            }
+            5 => {
+                crossplatform::replace_sound(
+                    "Old Minecraft Death Sound".to_string(),
+                    "mc".to_string(),
+                );
+                println!("Returning to main menu in 2 seconds..");
+                std::thread::sleep(std::time::Duration::from_secs(2));
+                main_menu()
+            }
+            6 => {
+                crossplatform::replace_sound("Lego Yoda Death".to_string(), "yoda".to_string());
+                println!("Returning to main menu in 2 seconds..");
+                std::thread::sleep(std::time::Duration::from_secs(2));
+                main_menu()
+            }
+            7 => {
+                crossplatform::replace_sound("AUUUUUUGHHH".to_string(), "augh".to_string());
+                println!("Returning to main menu in 2 seconds..");
+                std::thread::sleep(std::time::Duration::from_secs(2));
+                main_menu()
+            }
+            8 => {
+                crossplatform::custom_sound();
+                println!("Returning to main menu in 2 seconds..");
+                std::thread::sleep(std::time::Duration::from_secs(2));
+                main_menu()
+            }
+            9 => main_menu(),
+            _ => {
+                println!("Invalid input!\nReturning to main menu in 3 seconds...");
+                std::thread::sleep(std::time::Duration::from_secs(3));
+                main_menu()
+            }
         }
     }
 }
